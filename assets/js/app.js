68ef28c7562a405fe2c8dae864bfe191efcb8e84
alkachhirang <alkachhirang3@gmail.com>
2024-07-19 14:44:14 +0530

    add more-sections

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+//========= FOOTER-CURRENT-YEAR ======
+const currentYear = new Date().getFullYear();
+
+// Find the span element with the id 'year' and set its text content
+document.getElementById('year').textContent = currentYear;
+
