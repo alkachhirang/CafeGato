9850d6dd28d0bbeca346ffadd5f8e5587971f27d
alkachhirang <alkachhirang3@gmail.com>
2024-07-20 09:44:15 +0530

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,16 @@ const currentYear = new Date().getFullYear();
 // Find the span element with the id 'year' and set its text content
 document.getElementById('year').textContent = currentYear;
 
+//============ CONNECTED-FORM ===========
+document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const nameInput = event.target.querySelector('input[type="text"]');
+    const emailInput = event.target.querySelector('input[type="email"]');
+    const name = nameInput.value;
+    const email = emailInput.value;
+    console.log('Form submitted!');
+    console.log('Name:', name);
+    console.log('Email:', email);
+    nameInput.value = '';
+    emailInput.value = '';
+});
